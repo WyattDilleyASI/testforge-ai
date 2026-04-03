c14e940752fef025f650ab06922f38633d162cfa
WyattDilleyASI <wyatt.dilley@asirobots.com>
2026-04-03 15:32:31 -0600

    Update EasterEggs.jsx

--- client/src/components/EasterEggs.jsx
+++ client/src/components/EasterEggs.jsx
@@ -2670,8 +2670,8 @@ const CLIPPY_QUIPS = [
   "Let's make sure this will help the boots on the ground.",
   "Will this test end up with a bricked VCU?",
   "I'm Clippy, your useless TestForge Assistant!",
-  "Did you know that a John Deere 9RX could till 394,200,000 acres if ran continously for 900 years?"
-  "YouTube was initially created as a video-dating site. The more you know."
+  "Did you know that a John Deere 9RX could till 394,200,000 acres if ran continously for 900 years?",
+  "YouTube was initially created as a video-dating site. The more you know.",
 ];
 
 export const ClippyCompanion = () => {
